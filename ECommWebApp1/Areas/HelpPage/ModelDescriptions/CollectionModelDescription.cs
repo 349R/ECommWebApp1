3a30b34906433f5ae63ce1df056d9ab271bc5d32
349R <rcpayne@dslextreme.com>
2019-04-03 12:52:13 -0700

    Add project files.

---
+++ ECommWebApp1/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ECommWebApp1.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ECommWebApp1/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ECommWebApp1.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
